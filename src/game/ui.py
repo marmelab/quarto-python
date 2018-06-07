4cf29efad059c536f4f1766490c91f189a0b47c6
Julien Mattiussi <julienm@marmelab.com>
2018-06-07 17:32:26 +0200

    TERM not set doesn't appear anymore

--- src/game/ui.py
+++ src/game/ui.py
@@ -118,7 +118,6 @@ class UIRender:
 
     def clear_terminal(self):
         subprocess.call(["printf", "'\033c'"])
-        os.system('cls' if os.name == 'nt' else 'clear')
 
     def display_game(self, game_state):
         self.clear_terminal()
