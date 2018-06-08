1cd649663c61e5bbcfddb8d11e0f9c93d2ac4508
Julien Mattiussi <julienm@marmelab.com>
2018-06-08 12:09:41 +0200

    Review for player names creation

--- src/game/tools.py
+++ src/game/tools.py
@@ -7,8 +7,9 @@ class Players:
 
     """Definition of the users names:"""
 
-    player1_name = "Player 1"
-    player2_name = "Player 2"
+    def __init__(self, initial_state=""):
+        self.player1_name = "Player 1"
+        self.player2_name = "Player 2"
 
     def change_player_name(self, player_id, player_name):
         if player_id == 1:
